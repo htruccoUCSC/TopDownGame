f6f34b08b2fc30892bb056d34c8a3ab0e8aea9fe
avamalinowski <ava.malinowski@gmail.com>
2025-06-05 16:22:46 -0700

    made animations, added player to screen, made player move, played approparite animation

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -11,9 +11,50 @@ class Game extends Phaser.Scene {
         this.walkingLayer = this.map.createLayer("walking-layer", [this.lavaTileset, this.decoTileset], 0, 0);
         this.detailLayer = this.map.createLayer("detail-layer", [this.lavaTileset, this.decoTileset], 0, 0);
         this.miscLayer = this.map.createLayer("misc-layer", [this.lavaTileset, this.decoTileset], 0, 0);
+
+        my.sprite.player = this.physics.add.sprite(103, 50, "walk_down", 0);
+        my.sprite.player.setCollideWorldBounds(true);
+
+
+        this.aKey = this.input.keyboard.addKey('A');
+        this.wKey = this.input.keyboard.addKey('W');
+        this.sKey = this.input.keyboard.addKey('S');
+        this.dKey = this.input.keyboard.addKey('D');
     }
 
     update() {
+        let vx = 0;
+        let vy = 0;
+
+        if (this.aKey.isDown) vx -= 100;
+        if (this.dKey.isDown) vx += 100;
+        if (this.wKey.isDown) vy -= 100;
+        if (this.sKey.isDown) vy += 100;
+
+        my.sprite.player.setVelocity(vx, vy);
+        //walks left up
+        if (vx < 0 && vy < 0){
+            my.sprite.player.anims.play("walk_left_up", true);
+        //walks right up
+        } else if (vx > 0 && vy < 0){
+            my.sprite.player.anims.play("walk_right_up", true);
+        //walks up
+        } else if (vx == 0 && vy < 0){
+            my.sprite.player.anims.play("walk_back", true);
+        //walks down
+        } else if (vx == 0 && vy > 0){
+            my.sprite.player.anims.play("walk_down", true);
+        //walks left or walks left down
+        } else if ((vx < 0 && vy > 0) || (vx < 0 && vy == 0)){
+            my.sprite.player.anims.play("walk_left", true);
+        //walks right or walk right down
+        } else if ((vx > 0 && vy > 0) || (vx > 0 && vy == 0)){
+            my.sprite.player.anims.play("walk_right", true);
+        //plays the idle animation
+        } else {
+            my.sprite.player.anims.play("idle", true);
+        }
+    
         
     }
 }
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -5,6 +5,7 @@ class Preloader extends Phaser.Scene {
 
     preload() {
         this.load.setPath("./assets/");
+        //loading tile sheets
         this.load.spritesheet("lava_tiles", "freelavatileset-Sheet.png", {
             frameWidth: 16,
             frameHeight: 16
@@ -13,10 +14,80 @@ class Preloader extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         });
+        // loading character anims
+        this.load.spritesheet("walk_down", "walk_Down.png",{
+            frameWidth: 12,
+            frameHeight: 23        
+        });
+        //used for left and walk left down
+        this.load.spritesheet("walk_left", "walk_Left_Down.png",{
+            frameWidth: 16,
+            frameHeight: 25
+        });
+        this.load.spritesheet("walk_left_up", "walk_Left_Up.png",{
+            frameWidth: 15,
+            frameHeight: 24
+        });
+        //used for walk right and walk right down
+        this.load.spritesheet("walk_right", "walk_Right_Down.png",{
+            frameWidth: 16,
+            frameHeight: 26
+        });
+        this.load.spritesheet("walk_right_up", "walk_Right_Up.png",{
+            frameWidth: 16,
+            frameHeight: 24
+        });
+        this.load.spritesheet("walk_back", "walk_Up.png",{
+            frameWidth: 12,
+            frameHeight: 23
+        });
         this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj")
     }
 
     create() {
+        this.anims.create({
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("walk_down", {frames: [0]}),
+        });
+        this.anims.create({
+            key: "walk_down",
+            frames: this.anims.generateFrameNumbers("walk_down", {frames: [0,1,2,3,4,5,6]}),
+            frameRate: 15,
+            repeat: -1
+        });
+        //used for walking left and left down
+        this.anims.create({
+            key: "walk_left",
+            frames: this.anims.generateFrameNumbers("walk_left", {frames: [0,1,2,3,4,5,6,7]}),
+            frameRate: 15,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "walk_left_up",
+            frames: this.anims.generateFrameNumbers("walk_left_up", {frames: [0,1,2,3,4,5,6,7]}),
+            frameRate: 15,
+            repeat: -1
+        });
+        //used for walking right and right down
+        this.anims.create({
+            key: "walk_right",
+            frames: this.anims.generateFrameNumbers("walk_right", {frames: [0,1,2,3,4,5,6,7]}),
+            frameRate: 15,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "walk_right_up",
+            frames: this.anims.generateFrameNumbers("walk_right_up", {frames: [0,1,2,3,4,5,6,7]}),
+            frameRate: 15,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "walk_back",
+            frames: this.anims.generateFrameNumbers("walk_back", {frames: [0,1,2,3,4,5,6,7]}),
+            frameRate: 15,
+            repeat: -1
+        });
+
         this.scene.start("game");
     }  
 }
