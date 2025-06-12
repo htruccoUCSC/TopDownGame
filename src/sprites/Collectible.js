2da3fe1bb574bb31dc6c5a4e3d194e4d225ecaa3
avamalinowski <ava.malinowski@gmail.com>
2025-06-11 22:25:01 -0700

    adding health bar code for editing

--- src/sprites/Collectible.js
+++ src/sprites/Collectible.js
@@ -1,5 +1,42 @@
 class Collectible extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
+
+
+        //health bar
+        const numberOfHearts = this.health;
+        const hearts = [];
+        for (let i = 0; i < numberOfHearts; i++){
+            const heart = this.add.sprite(10 + i * 63,10, "tilemap_packed", 44).setScale(2).setOrigin(0);
+            hearts.push(heart);
+        }
+
+        this.heartObj = this.map.createFromObjects("Objects", {
+            name: "heart",
+            key: "tilemap_packed",
+            frame: 44
+        });
+
+        this.heartGroup = this.add.group();
+        this.heartObj.forEach(obj => {
+            this.physics.world.enable(obj, Phaser.Physics.Arcade.STATIC_BODY);
+            this.heartGroup.add(obj);
+        })
+
+        this.physics.add.overlap(my.sprite.player, this.heartGroup, (obj1, obj2) => {
+            if (this.health === 0){
+                return;
+            }
+            if (this.health === hearts.length){
+                obj2.destroy();
+                return;
+            }
+            console.log(this.health + "hello");
+            this.health += 1;
+            const heartIndex = this.health - 1;
+            hearts[heartIndex].play("gainHealth", true);
+            obj2.destroy();
+
+        });
     }
 }
